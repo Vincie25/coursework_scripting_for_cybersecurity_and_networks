7d3460ba1491465b6780c9081f603db083eb0159
Vincie25 <googlfreestyle@gmail.com>
2025-11-23 15:00:25 +0100

    new input

--- pcap_analyzer.py
+++ pcap_analyzer.py
@@ -1,35 +1,9 @@
 """used modules"""
 import socket
-import dpkt
+from pcap_reader import main
 
-def main(print_out=True, break_first=True) -> list:
-    """Going through the ethernetframe of the 1st package"""
-    pcapfile = "evidence-packet-analysis.pcap"
-    open_file = open(pcapfile, "rb")
-    pcap = dpkt.pcap.Reader(open_file)
-    packets = []
-    for unused_ts, buf in pcap:
-        # each tuple contains a timestamp
-        # prefixing the variable name with unused_ makes this clear and
-        # avoids pylint W0612: Unused Variable warning
-        eth = dpkt.ethernet.Ethernet(buf)
-        ip = eth.data
-        #tcp = ip.data
-        source_ip = ip.src
-        destination_ip = ip.dst
-        #source_port = tcp.sport
-        #destination_port = tcp.dport
-        details_source = f"{socket.inet_ntoa(source_ip)}"
-        details_destination = f"{socket.inet_ntoa(destination_ip)}"
-        details = f"{details_source} -> {details_destination}"
-        packets.append(details)
-        if break_first:  # stop after the first packet
-            break
-
-    open_file.close()
-    return packets
+def analyzer():
+    
 
 
 if __name__ == "__main__":
-    packets = main(print_out=False, break_first=True)
-    print(packets)
---
+++ pcap_reader.py
@@ -0,0 +1,40 @@
+"""import dpkt to read the in the frame"""
+import dpkt
+
+
+def main(print_out=True, break_first=True) -> list:
+    """Going through the ethernetframe of the 1st package"""
+    packets = []
+    try:
+        pcapfile = "evidence-packet-analysis.pcap"
+        open_file = open(pcapfile, "rb")
+        pcap = dpkt.pcap.Reader(open_file)
+        for unused_ts, buf in pcap:
+            # each tuple contains a timestamp
+            # prefixing the variable name with unused_ makes this clear and
+            # avoids pylint W0612: Unused Variable warning
+            eth = dpkt.ethernet.Ethernet(buf)
+            if print_out:
+                print(f"#<INFO> eth ethernet packet: {repr(eth)}\n")
+
+            ip_ad = eth.data
+            if print_out:
+                print(f"#<INFO> eth.data: {repr(ip_ad)}")
+
+            packets.append(eth)
+
+            if break_first:  # stop after the first packet
+                break    
+    except IOError:
+        print("File not found")
+    except dpkt.UnpackError:
+        print("Unable to unpack the file")
+    except Exception:
+        print("Parser error")
+    finally:
+        open_file.close()
+    return packets
+
+
+if __name__ == "__main__":
+    main()

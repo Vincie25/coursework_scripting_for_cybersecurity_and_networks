58e1ec1ff6dd5db5d86e4a431f86c2d513d30eca
Vincie25 <googlfreestyle@gmail.com>
2025-11-23 10:52:42 +0100

    reading the ethernetframe of the 1st packagegi

--- pcap_analyzer.py
+++ pcap_analyzer.py
@@ -1,15 +1,30 @@
 import dpkt
 
-print("File loaded…")
-print("Graph saved…")
-print("Results written…")
-PCAPFILE = "evidence-packet-analysis.pcap"
-open_file = open(PCAPFILE, "rb")
-pcap = dpkt.pcap.Reader(open_file)
-for unused_ts, buf in pcap:
-    # each tuple contains a timestamp
-    # prefixing the variable name with unused_ makes this clear and
-    # avoids pylint W0612: Unused Variable warning
-    eth = dpkt.ethernet.Ethernet(buf)
+def main(print_out=True, break_first=True) -> list:
+    """Going through the ethernetframe of the 1st package"""
+    pcapfile = "evidence-packet-analysis.pcap"
+    open_file = open(pcapfile, "rb")
+    pcap = dpkt.pcap.Reader(open_file)
+    packets = []
+    for unused_ts, buf in pcap:
+        # each tuple contains a timestamp
+        # prefixing the variable name with unused_ makes this clear and
+        # avoids pylint W0612: Unused Variable warning
+        eth = dpkt.ethernet.Ethernet(buf)
+        if print_out:
+            print(f"#<INFO> eth ethernet packet: {repr(eth)}\n")
 
-open_file.close()
+        ip_ad = eth.data
+        if print_out:
+            print(f"#<INFO> eth.data: {repr(ip_ad)}")
+
+        packets.append(eth)
+
+        if break_first:  # stop after the first packet
+            break
+
+    open_file.close()
+    return packets
+
+if __name__ == "__main__":
+    main()

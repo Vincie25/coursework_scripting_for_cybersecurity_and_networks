c0b0eb63c5cbce8e6e2161b470170438aa0193d1
Vincie25 <googlfreestyle@gmail.com>
2025-11-24 12:34:41 +0100

    update stats

--- pcap_stats.py
+++ pcap_stats.py
@@ -1,11 +1,11 @@
 """Used libaries"""
 import dpkt
 from pcap_reader import main
-from datetime import datetime
+
 
 def stats() ->None:
+    """total amount of packages and there type"""
     packets = main(print_out=False, break_first=False)
-    ipcounter = 0
     tcpcounter = 0
     udpcounter = 0
     igmcounter = 0
@@ -15,22 +15,21 @@ def stats() ->None:
     for ts, pkt in packets:
         eth =  pkt
         if eth.type==dpkt.ethernet.ETH_TYPE_IP:
-            ipcounter+=1
-        ip =eth.data
-        if ip.p==dpkt.ip.IP_PROTO_TCP:
-            tcpcounter+=1
-            tcp_timestamps.append(ts)
-        if ip.p==dpkt.ip.IP_PROTO_UDP:
-            udpcounter+=1
-            udp_timestamps.append(ts)
-        if ip.p==dpkt.ip.IP_PROTO_IGMP:
-            igmcounter+=1
-            igm_timestamps.append(ts)
+            ip =eth.data
+            if ip.p==dpkt.ip.IP_PROTO_TCP:
+                tcpcounter+=1
+                tcp_timestamps.append(ts)
+            elif ip.p==dpkt.ip.IP_PROTO_UDP:
+                udpcounter+=1
+                udp_timestamps.append(ts)
+            elif ip.p==dpkt.ip.IP_PROTO_IGMP:
+                igmcounter+=1
+                igm_timestamps.append(ts)
     tcp_timestamps.sort()
     udp_timestamps.sort()
     igm_timestamps.sort()
 
-    print(f"{ipcounter}")
+    print(f"{len(packets)}")
     print(f"{tcpcounter} {tcp_timestamps[0]} {tcp_timestamps[-1]}")
     print(f"{udpcounter} {udp_timestamps[0]} {udp_timestamps[-1]}")
     print(f"{igmcounter} {igm_timestamps[0]} {igm_timestamps[-1]}")

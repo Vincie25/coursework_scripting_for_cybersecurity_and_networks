c722799271c00eb71879d1d2a53eb0e995734c25
Vincie25 <googlfreestyle@gmail.com>
2025-11-24 12:51:10 +0100

    updating pcap analyzer

--- pcap_analyzer.py
+++ pcap_analyzer.py
@@ -1,22 +1,24 @@
 """used modules"""
 import socket
+import dpkt
 from pcap_reader import main
 
 def analyzer() -> None:
     """Analyzing readed Data"""
     packets = main(print_out=False, break_first=False)
+    flows:dict = {}
 
     ## Add your code here
-    for i in packets:
+    for unused_ts, i in packets:
         eth = i
         ip = eth.data
-        #tcp = ip.data
         source_ip = ip.src
         destination_ip = ip.dst
-        #source_port = tcp.sport
-        #destination_port = tcp.dport
         details = f"{socket.inet_ntoa(source_ip)} -> {socket.inet_ntoa(destination_ip)}"
-        print(f"Connection details: {details}")
+        flows[details] = flows.get(details, 0) + 1
+        sorted_flows = sorted(flows.items(), key=lambda x: x[1], reverse=True)
+        for key, value in sorted_flows:
+            print(f"{key} - {value}")
 
 if __name__ == "__main__":
     analyzer()

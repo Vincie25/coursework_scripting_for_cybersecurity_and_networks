708c5a13b1591448c7b1d59d17f9277055f3aa6f
Vincie25 <googlfreestyle@gmail.com>
2025-11-27 12:35:05 +0100

    update stats

--- pcap_stats.py
+++ pcap_stats.py
@@ -6,34 +6,36 @@ from pcap_reader import main
 def stats() ->None:
     """total amount of packages and there type"""
     packets = main(print_out=False, break_first=False)
-    tcpcounter = 0
-    udpcounter = 0
-    igmcounter = 0
-    tcp_timestamps: list = []
-    udp_timestamps: list = []
-    igm_timestamps: list = []
+    protocols = {
+    dpkt.ip.IP_PROTO_TCP: {
+        "name": "TCP",
+        "counter": 0,
+        "timestamps": []
+    },
+    dpkt.ip.IP_PROTO_UDP: {
+        "name": "UDP",
+        "counter": 0,
+        "timestamps": []
+    },
+    dpkt.ip.IP_PROTO_IGMP: {
+        "name": "IGMP",
+        "counter": 0,
+        "timestamps": []
+    }
+}
     for ts, pkt in packets:
-        eth =  pkt
+        eth = pkt
         if eth.type==dpkt.ethernet.ETH_TYPE_IP:
             ip =eth.data
-            if ip.p==dpkt.ip.IP_PROTO_TCP:
-                tcpcounter+=1
-                tcp_timestamps.append(ts)
-            elif ip.p==dpkt.ip.IP_PROTO_UDP:
-                udpcounter+=1
-                udp_timestamps.append(ts)
-            elif ip.p==dpkt.ip.IP_PROTO_IGMP:
-                igmcounter+=1
-                igm_timestamps.append(ts)
-    tcp_timestamps.sort()
-    udp_timestamps.sort()
-    igm_timestamps.sort()
-
-    print(f"{len(packets)}")
-    print(f"{tcpcounter} {tcp_timestamps[0]} {tcp_timestamps[-1]}")
-    print(f"{udpcounter} {udp_timestamps[0]} {udp_timestamps[-1]}")
-    print(f"{igmcounter} {igm_timestamps[0]} {igm_timestamps[-1]}")
-
+            if ip.p in protocols:
+                protocols[ip.p]["counter"]+=1
+                protocols[ip.p]["timestamps"].append(ts)
+    mean_value = len(pkt)
+    print(f"The mean value: {mean_value}")
+    for unused_key, data in protocols.items():
+        if data["timestamps"]:  # Nur wenn Pakete vorhanden
+            data["timestamps"].sort()
+            print(f"{data['name']}: {data['counter']}, {data['timestamps'][0]}, {data['timestamps'][-1]}")
 
 if __name__ == "__main__":
     stats()

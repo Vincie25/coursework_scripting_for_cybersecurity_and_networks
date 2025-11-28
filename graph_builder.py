5fe9b10d16e4ea6922279fcc6de80c4cee86e6d3
Vincie25 <googlfreestyle@gmail.com>
2025-11-28 20:06:53 +0100

    update

--- graph_builder.py
+++ graph_builder.py
@@ -3,18 +3,23 @@ import socket
 import matplotlib.pyplot as plt 
 import networkx as nx
 from pcap_reader import main
+from pcap_analyzer import analyzer
 
 
-plt.figure(figsize=(28,12))
-packets = main(print_out=False, break_first=False)
-IP = nx.DiGraph()
-## Add your code here
-for unused_ts, i in packets:
-    eth = i
-    ip = eth.data
-    tcp = ip.data
-    source_ip = ip.src
-    destination_ip = ip.dst
-    IP.add_edges_from([(socket.inet_ntoa(destination_ip), socket.inet_ntoa(source_ip))])
-nx.draw_networkx(IP, pos=nx.shell_layout(IP))
-plt.show()
+def graph() -> None:
+    plt.figure(figsize=(28,12))
+    packets = main(print_out=False, break_first=False)
+    IP = nx.DiGraph()
+    ## Add your code here
+    for unused_ts, i in packets:
+        eth = i
+        ip = eth.data
+        tcp = ip.data
+        source_ip = ip.src
+        destination_ip = ip.dst
+        IP.add_edges_from([(socket.inet_ntoa(destination_ip), socket.inet_ntoa(source_ip))])
+    nx.draw_networkx(IP, pos=nx.shell_layout(IP))
+    plt.show()
+
+if __name__ == "__main__":
+    graph()
--- time_plotter.py
+++ time_plotter.py
@@ -1,24 +1,29 @@
 import dpkt
+import pandas as pd
 import matplotlib.pyplot as plt
-import socket
+import numpy as np
 from pcap_reader import main
 
 
 def time_plot():
     packets = main(print_out=False, break_first=False)
     data_type = 'size'
+    data = {'Time':[],
+            'Data to plot':[],}
+    data_df = pd.DataFrame(data)
     timestamps = []
     data_to_plot = []
     for ts, pkt in packets:
         eth = pkt
         ip = eth.data
         if isinstance(ip, dpkt.ip.IP):
-            timestamps.append(ts)
+            data['Time'].append(ts)
             if data_type == 'size':
-                data_to_plot.append(len(pkt))
+                data['Data to plot'].append(len(pkt))
     if data_type == 'size':
+        data_df,
         plt.figure(figsize=(12, 6))
-        plt.plot(timestamps, data_to_plot)
+        plt.plot(data['Time'], data['Data to plot'])
         plt.xlabel("Timestamp")
         plt.ylabel("Packet Size (bytes)")
         plt.title("Packet Size over Time")

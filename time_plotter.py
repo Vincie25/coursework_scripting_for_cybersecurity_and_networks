0973e54e9ddab3f472f863fcdd3a194dd762d1d8
Vincie25 <googlfreestyle@gmail.com>
2025-11-29 19:50:41 +0100

    updating timeplot

--- time_plotter.py
+++ time_plotter.py
@@ -1,11 +1,12 @@
+"""Needed modules"""
 from datetime import timedelta
+from statistics import mean, stdev
 import matplotlib.pyplot as plt
 from pcap_reader import main
 
-
 def time_plot():
     packets = main(print_out=False, break_first=False)
-    interval_size = timedelta(seconds=1)
+    interval_size = timedelta(seconds=5)
     start = packets[0][0]
     end = start + interval_size
     count = 0
@@ -21,6 +22,9 @@ def time_plot():
             end = start + interval_size
             count = 1
     plt.plot(interval_times, interval_counts)
+    threshold = mean(interval_counts) + 2*stdev(interval_counts)
+    plt.axhline(y=threshold)
+    plt.savefig("timeplot1.png")
     plt.show()
 
 

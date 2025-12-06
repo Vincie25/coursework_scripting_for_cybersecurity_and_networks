50a09d9d001800853f07ce2623710e42312e02a7
Vincie25 <googlfreestyle@gmail.com>
2025-12-06 23:07:03 +0100

    more styling

--- pcap_analyzer.py
+++ pcap_analyzer.py
@@ -10,9 +10,9 @@ def analyzer(pcap) -> None:
     for unused_ts, i in pcap:
         eth = i
         ip = eth.data
-        source_ip = ip.src
-        destination_ip = ip.dst
-        details = f"{socket.inet_ntoa(source_ip)} -> {socket.inet_ntoa(destination_ip)}"
+        src_ip = ip.src
+        dst_ip = ip.dst
+        details = f"{socket.inet_ntoa(src_ip)} -> {socket.inet_ntoa(dst_ip)}"
         flows[details] = flows.get(details, 0) + 1
         sorted_flows = sorted(flows.items(), key=lambda x: x[1], reverse=True)
         for key, value in sorted_flows:

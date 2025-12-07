85b456a3978d08d500ab371c429e0ef728b98c15
Vincie25 <googlfreestyle@gmail.com>
2025-12-07 11:06:38 +0100

    styling

--- pcap_reader.py
+++ pcap_reader.py
@@ -13,25 +13,22 @@ def main(pcapfile: str, printout: bool = True, brkfirst: bool = True) -> list:
     and the decoded Ethernet frame."""
     packets = []
     try:
-        open_file = open(pcapfile, "rb")
-        pcap = dpkt.pcap.Reader(open_file)
-        sys.stderr.write("File opened")
-        for ts, buf in pcap:
-            # each tuple contains a timestamp
-            # prefixing the variable name with unused_ makes this clear and
-            # avoids pylint W0612: Unused Variable warning
-            eth = dpkt.ethernet.Ethernet(buf)
-            if printout:
-                sys.stderr.write(f"#<INFO> eth ethernet packet: {repr(eth)}\n")
-
-            ip_ad = eth.data
-            if printout:
-                sys.stderr.write(f"#<INFO> eth.data: {repr(ip_ad)}")
-
-            packets.append((datetime.fromtimestamp(ts), eth))
-
-            if brkfirst:  # stop after the first packet
-                break
+        with open(pcapfile, "rb") as open_file:
+            pcap = dpkt.pcap.Reader(open_file)
+            sys.stderr.write("File opened")
+            for ts, buf in pcap:
+                # each tuple contains a timestamp
+                # prefixing the variable name with unused_ makes this clear and
+                # avoids pylint W0612: Unused Variable warning
+                eth = dpkt.ethernet.Ethernet(buf)
+                if printout:
+                    sys.stderr.write(f"#<INFO> eth ethernet packet: {repr(eth)}\n")
+                ip_ad = eth.data
+                if printout:
+                    sys.stderr.write(f"#<INFO> eth.data: {repr(ip_ad)}")
+                packets.append((datetime.fromtimestamp(ts), eth))
+                if brkfirst:  # stop after the first packet
+                    break
     except IOError:
         sys.stderr.write("File not found")
     except dpkt.UnpackError:
@@ -39,7 +36,6 @@ def main(pcapfile: str, printout: bool = True, brkfirst: bool = True) -> list:
     except Exception:
         sys.stderr.write("Parser error")
     finally:
-        open_file.close()
         sys.stderr.write("File closed")
     return packets
 
